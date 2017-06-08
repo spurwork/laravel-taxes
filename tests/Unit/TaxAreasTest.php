63a93550329290c62a889bf8fd4fd7d9b683bfbb
Richard McDaniel <richard.mcdaniel@uah.edu>
2017-06-08 19:10:52 +0000

    Add Attalla

--- tests/Unit/TaxAreasTest.php
+++ tests/Unit/TaxAreasTest.php
@@ -10,10 +10,22 @@ class TaxAreasTest extends \TestCase
     {
         parent::setUp();
         $this->artisan('migrate');
+        $this->artisan('db:seed', ['--class' => 'Appleton\Taxes\Seeds\AttallaSeeder', '--database' => 'testing']);
         $this->artisan('db:seed', ['--class' => 'Appleton\Taxes\Seeds\BirminghamSeeder', '--database' => 'testing']);
     }
 
-    public function testTaxAreas()
+    public function testAttalla()
+    {
+        $result = TaxArea::atPoint(34.0218, -86.0886)
+            ->first()
+            ->tax
+            ->withEarnings(2300)
+            ->compute();
+
+        $this->assertSame(46.00, $result);
+    }
+
+    public function testBirmingham()
     {
         $result = TaxArea::atPoint(33.5207, -86.8025)
             ->first()
