a8d43cb966d076639963f676b0153368ae3da278
Richard McDaniel <richard.mcdaniel@uah.edu>
2017-06-08 14:44:38 +0000

    Add HasTaxRate trait

--- src/Countries/US/Alabama/BirminghamOccupational.php
+++ src/Countries/US/Alabama/BirminghamOccupational.php
@@ -3,14 +3,11 @@
 namespace Appleton\Taxes\Countries\US\Alabama;
 
 use Appleton\Taxes\Classes\BaseTax;
-use Appleton\Taxes\Traits\HasWageBase;
+use Appleton\Taxes\Traits\HasTaxRate;
 
 class BirminghamOccupational extends BaseTax
 {
-    const TAX_RATE = 0.01;
+    use HasTaxRate;
 
-    public function compute()
-    {
-        return round($this->earnings() * self::TAX_RATE, 2);
-    }
+    const TAX_RATE = 0.01;
 }
