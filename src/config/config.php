ceead5dbcf2e012d6c06dd213d55d30ca992f885
Richard McDaniel <richard.mcdaniel@uah.edu>
2017-06-16 22:43:20 +0000

    Get names from env

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'governmental_unit_areas' => 'governmental_unit_areas',
-    'tax_areas' => 'tax_areas',
+    'governmental_unit_areas' => env('TAXES_GOVERNMENTAL_UNIT_AREAS', 'governmental_unit_areas'),
+    'tax_areas' => env('TAXES_TAX_AREAS', 'tax_areas'),
 ];
