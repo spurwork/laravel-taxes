445612f3d2df7d716008ddc1190451b73ecc1ed4
Richard McDaniel <richard.mcdaniel@uah.edu>
2017-06-21 21:18:51 +0000

    More tests

--- tests/Unit/Commands/ImportCommandTest.php
+++ tests/Unit/Commands/ImportCommandTest.php
@@ -4,6 +4,7 @@ namespace Appleton\Taxes\Commands;
 
 use Appleton\Taxes\Models\GovernmentalUnitArea;
 use Appleton\Taxes\Models\TaxArea;
+use Artisan;
 
 class ImportCommandTest extends \TestCase
 {
@@ -19,5 +20,30 @@ class ImportCommandTest extends \TestCase
         $this->artisan('taxes:import', ['country' => 'US']);
         $this->assertSame(27, GovernmentalUnitArea::count());
         $this->assertSame(33, TaxArea::count());
+    }
+
+    public function testImportUSTwice()
+    {
+        $this->artisan('taxes:import', ['country' => 'US']);
+        $this->artisan('taxes:import', ['country' => 'US']);
+        $this->assertSame(27, GovernmentalUnitArea::count());
+        $this->assertSame(33, TaxArea::count());
+    }
+
+    public function testImportNoCountry()
+    {
+        $this->expectExceptionMessage('Not enough arguments (missing: "country").');
+
+        $this->artisan('taxes:import');
+        $this->assertSame(0, GovernmentalUnitArea::count());
+        $this->assertSame(0, TaxArea::count());
+    }
+
+    public function testImportBadCountry()
+    {
+        $this->artisan('taxes:import', ['country' => 'XX']);
+        $this->assertSame(0, GovernmentalUnitArea::count());
+        $this->assertSame(0, TaxArea::count());
+        $this->assertSame("No importer found for specified country.\n", Artisan::output());
     }
 }
