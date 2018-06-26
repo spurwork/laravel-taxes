c1875b686e041778352ae2e6d232f5e3aab49369
cmhand <soccerdebater@gmail.com>
2018-06-26 07:32:12 -0500

    Add tests.

--- tests/Unit/Countries/US/Wisconsin/WisconsinIncomeTest.php
+++ tests/Unit/Countries/US/Wisconsin/WisconsinIncomeTest.php
@@ -2,6 +2,7 @@
 
 namespace Appleton\Taxes\Countries\US\Wisconsin\WisconsinIncome;
 
+use Appleton\Taxes\Models\Countries\US\Wisconsin\WisconsinIncomeTaxInformation;
 use Carbon\Carbon;
 
 class WisconsinIncomeTest extends \TestCase
@@ -26,6 +27,30 @@ class WisconsinIncomeTest extends \TestCase
         });
 
         $this->assertSame(56.87, $results->getTax(WisconsinIncome::class));
+
+        WisconsinIncomeTaxInformation::forUser($this->user)->update(['filing_status' => WisconsinIncome::FILING_MARRIED]);
+
+        $results = $this->taxes->calculate(function ($taxes) {
+            $taxes->setHomeLocation($this->getLocation('us.wisconsin'));
+            $taxes->setWorkLocation($this->getLocation('us.wisconsin'));
+            $taxes->setUser($this->user);
+            $taxes->setEarnings(1000);
+            $taxes->setPayPeriods(52);
+        });
+
+        $this->assertSame(54.92, $results->getTax(WisconsinIncome::class));
+
+        WisconsinIncomeTaxInformation::forUser($this->user)->update(['filing_status' => WisconsinIncome::FILING_SEPERATE]);
+
+        $results = $this->taxes->calculate(function ($taxes) {
+            $taxes->setHomeLocation($this->getLocation('us.wisconsin'));
+            $taxes->setWorkLocation($this->getLocation('us.wisconsin'));
+            $taxes->setUser($this->user);
+            $taxes->setEarnings(1000);
+            $taxes->setPayPeriods(52);
+        });
+
+        $this->assertSame(58.81, $results->getTax(WisconsinIncome::class));
     }
-    
+
 }
