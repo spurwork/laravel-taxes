a97095f82f0377f4ada411a4230ce20e3d05e74c
Richard McDaniel <richard.mcdaniel@uah.edu>
2019-01-18 15:36:54 +0000

    Add MA WTF tax

---
+++ src/Countries/US/Massachusetts/MassachusettsWorkforceTrainingFund/MassachusettsWorkforceTrainingFund.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\Massachusetts\MassachusettsWorkforceTrainingFund;
+
+use Appleton\Taxes\Classes\BaseTax;
+
+abstract class MassachusettsWorkforceTrainingFund extends BaseTax
+{
+    const TYPE = 'state';
+    const WITHHELD = false;
+}
