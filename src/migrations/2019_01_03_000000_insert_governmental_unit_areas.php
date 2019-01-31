8bf797b41884fda1bfe3857a3a3cffcf1f3951da
steve <steve@spurjobs.com>
2019-01-31 16:45:20 -0600

    Adding in the geo areas for the rest of the states

---
+++ src/migrations/2019_01_03_000000_insert_governmental_unit_areas.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class InsertGovernmentalUnitAreas extends Migration
+{
+    private const STATE_FILE = '2019_01_03_000000_states.ini';
+    private $governmental_unit_areas = 'governmental_unit_areas';
+
+    public function up()
+    {
+        $states = parse_ini_file(self::STATE_FILE);
+
+        foreach ($states as $state_name => $state_geo) {
+            if (!DB::table($this->governmental_unit_areas)->where('name', $state_name)->exists()) {
+                DB::table($this->governmental_unit_areas)->insertGetId(
+                    [
+                        'name' => $state_name,
+                        'area' => $state_geo,
+                    ]
+                );
+            }
+        }
+    }
+
+    public function down()
+    {
+        foreach (parse_ini_file(self::STATE_FILE) as $state_name => $state_geo) {
+            DB::table($this->governmental_unit_areas)->where('name', $state_name)->delete();
+        }
+    }
+}
