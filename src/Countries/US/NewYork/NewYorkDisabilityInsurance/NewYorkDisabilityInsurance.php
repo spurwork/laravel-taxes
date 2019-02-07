595135d73c345d9ed9592d4ab3463418e6c036da
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2019-02-07 09:33:46 -0600

    Add NY DIT (#71)

---
+++ src/Countries/US/NewYork/NewYorkDisabilityInsurance/NewYorkDisabilityInsurance.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\NewYork\NewYorkDisabilityInsurance;
+
+use Appleton\Taxes\Classes\BaseTax;
+
+abstract class NewYorkDisabilityInsurance extends BaseTax
+{
+    const TYPE = 'state';
+}
---
+++ tests/Unit/Countries/US/NewYork/NewYorkDisabilityInsuranceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\NewYork\NewYorkDisabilityInsurance;
+
+use Carbon\Carbon;
+
+class NewYorkDisabilityInsuranceTest extends \TestCase
+{
+    /**
+     * @dataProvider provideTestData
+     */
+    public function testNewYorkDisabilityInsurance($date, $wtd_earnings, $earnings, $result)
+    {
+        Carbon::setTestNow(
+            Carbon::parse($date, 'America/Chicago')->setTimezone('UTC')
+        );
+
+        $results = $this->taxes->calculate(function ($taxes) use ($wtd_earnings, $earnings) {
+            $taxes->setHomeLocation($this->getLocation('us.new_york'));
+            $taxes->setWorkLocation($this->getLocation('us.new_york'));
+            $taxes->setUser($this->user);
+            $taxes->setWtdEarnings($wtd_earnings);
+            $taxes->setEarnings($earnings);
+        });
+
+        $this->assertSame($result, $results->getTax(NewYorkDisabilityInsurance::class));
+    }
+
+    public function provideTestData()
+    {
+        return [
+            '0' => [
+                'January 1, 2019 8am',
+                0,
+                350,
+                0.60,
+            ],
+            '1' => [
+                'January 1, 2019 8am',
+                0,
+                1400,
+                0.60,
+            ],
+            '2' => [
+                'January 1, 2019 8am',
+                0,
+                100,
+                0.50,
+            ],
+        ];
+    }
+}
