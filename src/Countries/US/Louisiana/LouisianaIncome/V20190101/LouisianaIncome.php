8764557308ad62f7d2b5719122c6f7e4701144bf
Aidan Sullivan <aidancs316@gmail.com>
2019-06-19 08:41:02 -0500

    ACC-246 refactor income

--- src/Countries/US/Louisiana/LouisianaIncome/V20190101/LouisianaIncome.php
+++ src/Countries/US/Louisiana/LouisianaIncome/V20190101/LouisianaIncome.php
@@ -8,8 +8,11 @@ use Illuminate\Database\Eloquent\Collection;
 
 class LouisianaIncome extends BaseLouisianaIncome
 {
-    const SUPPLEMENTAL_TAX_RATE = 0;
     const TAX_RATE = 0.04;
+
+    public $min_income_amount;
+    public $max_income_amount;
+    public $multiplier;
 
     public function __construct(LouisianaIncomeTaxInformation $tax_information, Payroll $payroll)
     {
@@ -32,19 +35,39 @@ class LouisianaIncome extends BaseLouisianaIncome
         return [[0, self::TAX_RATE, 0]];
     }
 
-    public function dependents()
+    public function getAdjustedEarnings(): int
+    {
+        $gross_wages = $this->payroll->getEarnings() * $this->payroll->pay_periods;
+        return $gross_wages;
+    }
+
+    private function dependents(): int
     {
         return $this->tax_information->dependents * 1000;
     }
 
-    public function personalExemptions()
+    private function personalExemptions(): int
     {
         return $this->tax_information->exemptions * 4500;
     }
 
-    public function getPartA()
+    private function getMaritalStatus(): void
+    {
+        if ($this->tax_information->filing_status === 'M') {
+            $this->min_income_amount = 25000;
+            $this->max_income_amount = 100000;
+            $this->multiplier = .0165;
+        } else {
+            $this->min_income_amount = 12500;
+            $this->max_income_amount = 50000;
+            $this->multiplier = .016;
+        }
+    }
+
+    private function getPartA(): float
     {
         $sub_total = $this->dependents() + $this->personalExemptions();
+
         if ($sub_total < 0) {
             return 0;
         }
@@ -54,52 +77,35 @@ class LouisianaIncome extends BaseLouisianaIncome
         return $total > 0 ? $total : 0;
     }
 
-    public function getPartB()
+    private function getPartB(): float
     {
-        if ($this->tax_information->filing_status === 'M') {
-            $total = (($this->dependents() + $this->personalExemptions()) - 25000);
+        $total = (($this->dependents() + $this->personalExemptions()) - $this->min_income_amount);
 
-            if ($total < 0) {
-                return 0;
-            }
-
-            return (0.0165 * ($total / $this->payroll->pay_periods));
-        } else {
-            $total = (($this->dependents() + $this->personalExemptions()) - 12500);
-
-            if ($total < 0) {
-                return 0;
-            }
-
-            return (0.016 * ($total / $this->payroll->pay_periods));
+        if ($total < 0) {
+            return 0;
         }
+
+        return ($this->multiplier * ($total / $this->payroll->pay_periods));
     }
 
-    public function getWithholdingTaxPerPayPeriod()
+    private function getWithholdingTaxPerPayPeriod(): float
     {
-        if ($this->tax_information->filing_status === 'S') {
-            if ($this->getAdjustedEarnings() <= 12500) {
-                $total = (0.021 * $this->payroll->getEarnings()) - ($this->getPartA() + $this->getPartB());
-            } elseif ($this->getAdjustedEarnings() > 12500 && $this->getAdjustedEarnings() <= 50000) {
-                $total = (0.021 * $this->payroll->getEarnings()) + (0.016 * ($this->payroll->getEarnings() - (12500 / $this->payroll->pay_periods))) - ($this->getPartA() + $this->getPartB());
-            } elseif ($this->getAdjustedEarnings() > 50000) {
-                $total = (0.021 * $this->payroll->getEarnings()) + (0.016 * ($this->payroll->getEarnings() - (12500 / $this->payroll->pay_periods))) + (0.0135 * ($this->payroll->getEarnings() - (50000 / $this->payroll->pay_periods))) - ($this->getPartA() + $this->getPartB());
-            }
-        } else {
-            if ($this->getAdjustedEarnings() <= 25000) {
-                $total = (0.021 * $this->payroll->getEarnings()) - ($this->getPartA() + $this->getPartB());
-            } elseif ($this->getAdjustedEarnings() > 25000 && $this->getAdjustedEarnings() <= 100000) {
-                $total = (0.021 * $this->payroll->getEarnings()) + (0.0165 * ($this->payroll->getEarnings() - (25000 / $this->payroll->pay_periods))) - ($this->getPartA() + $this->getPartB());
-            } elseif ($this->getAdjustedEarnings() > 100000) {
-                $total = (0.021 * $this->payroll->getEarnings()) + (0.0165 * ($this->payroll->getEarnings() - (25000 / $this->payroll->pay_periods))) + (0.0135 * ($this->payroll->getEarnings() - (100000 / $this->payroll->pay_periods))) - ($this->getPartA() + $this->getPartB());
-            }
+        $this->getMaritalStatus();
+
+        $total = (0.021 * $this->payroll->getEarnings()) - ($this->getPartA() + $this->getPartB());
+
+        if ($this->getAdjustedEarnings() <= $this->min_income_amount) {
+            return $total > 0 ? $total : 0;
         }
+
+        if ($this->getAdjustedEarnings() > $this->min_income_amount) {
+            $total += ($this->multiplier * ($this->payroll->getEarnings() - ($this->min_income_amount / $this->payroll->pay_periods)));
+        }
+
+        if ($this->getAdjustedEarnings() > $this->max_income_amount) {
+            $total += (0.0135 * ($this->payroll->getEarnings() - ($this->max_income_amount / $this->payroll->pay_periods)));
+        }
+
         return $total > 0 ? $total : 0;
-    }
-
-    public function getAdjustedEarnings(): int
-    {
-        $gross_wages = $this->payroll->getEarnings() * $this->payroll->pay_periods;
-        return $gross_wages;
     }
 }
