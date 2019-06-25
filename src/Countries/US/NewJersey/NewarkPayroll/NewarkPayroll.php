805c5453091b8f3cc95218af3dcee637268aa27c
Matthew Petty <maybenull@gmail.com>
2019-06-25 10:17:57 -0500

    ACC-228 added newark payroll tax

---
+++ src/Countries/US/NewJersey/NewarkPayroll/NewarkPayroll.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\NewJersey\NewarkPayroll;
+
+use Appleton\Taxes\Classes\BaseLocal;
+
+abstract class NewarkPayroll extends BaseLocal
+{
+    const WITHHELD = false;
+}
