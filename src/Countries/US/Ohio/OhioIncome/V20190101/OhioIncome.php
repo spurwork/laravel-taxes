c64a4518676e787998d64f221bdefaa7e77ae411
Aidan Sullivan <aidancs316@gmail.com>
2019-06-27 13:40:51 -0500

    ACC-123 income and unemployment classes

---
+++ src/Countries/US/Ohio/OhioIncome/V20190101/OhioIncome.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\Ohio\OhioIncome\V20190101;
+
+use Appleton\Taxes\Classes\Payroll;
+use Appleton\Taxes\Countries\US\Ohio\OhioIncome\OhioIncome as BaseOhioIncome;
+use Appleton\Taxes\Models\Countries\US\Ohio\OhioIncomeTaxInformation;
+use Illuminate\Database\Eloquent\Collection;
+
+class OhioIncome extends BaseOhioIncome
+{
+    const SUPPLEMENTAL_TAX_RATE = 0.0535;
+
+    const TAX_RATE = 0.0535;
+
+    const STANDARD_DEDUCTIONS = [
+        self::FILING_SINGLE => 10000,
+        self::FILING_HEAD_OF_HOUSEHOLD => 15000,
+        self::FILING_MARRIED => 10000,
+        self::FILING_SEPERATE => 10000,
+    ];
+
+    const DEPENDENT_EXEMPTION_BRACKETS = [
+        self::FILING_SINGLE => [
+            [0, 2500],
+            [20000, 2000],
+            [30000, 1500],
+            [40000, 1000],
+            [50000, 500],
+            [60000, 0],
+        ],
+        self::FILING_HEAD_OF_HOUSEHOLD => [
+            [0, 2500],
+            [30000, 2000],
+            [45000, 1500],
+            [60000, 1000],
+            [75000, 500],
+            [90000, 0],
+        ],
+        self::FILING_MARRIED => [
+            [0, 2500],
+            [40000, 2000],
+            [60000, 1500],
+            [80000, 1000],
+            [100000, 500],
+            [120000, 0],
+        ],
+        self::FILING_SEPERATE => [
+            [0, 2500],
+            [20000, 2000],
+            [30000, 1500],
+            [40000, 1000],
+            [50000, 500],
+            [60000, 0],
+        ],
+    ];
+
+    public function __construct(OhioIncomeTaxInformation $tax_information, Payroll $payroll)
+    {
+        parent::__construct($payroll);
+        $this->tax_information = $tax_information;
+    }
+
+    public function getAdjustedEarnings()
+    {
+        return $this->getGrossEarnings() - $this->getStandardDeduction() - $this->getDependentExemption();
+    }
+
+    public function getSupplementalIncomeTax()
+    {
+        return $this->payroll->getSupplementalEarnings() * self::SUPPLEMENTAL_TAX_RATE;
+    }
+
+    public function getTaxBrackets()
+    {
+        return [[0, self::TAX_RATE, 0]];
+    }
+
+    public function compute(Collection $tax_areas)
+    {
+        $result = parent::compute($tax_areas);
+        return round($result, 0);
+    }
+
+    private function getStandardDeduction()
+    {
+        if (array_key_exists($this->tax_information->filing_status, static::STANDARD_DEDUCTIONS)) {
+            return static::STANDARD_DEDUCTIONS[$this->tax_information->filing_status];
+        }
+
+        return 0;
+    }
+
+    private function getDependentExemption()
+    {
+        $gross_earnings = $this->getGrossEarnings();
+        $dependent_exemption = $this->getTaxBracket($gross_earnings, static::DEPENDENT_EXEMPTION_BRACKETS[$this->tax_information->filing_status]);
+        return $this->tax_information->dependents * $dependent_exemption[1];
+    }
+
+    private function getGrossEarnings()
+    {
+        return ($this->payroll->getEarnings() - $this->payroll->getSupplementalEarnings()) * $this->payroll->pay_periods;
+    }
+}
---
+++ src/Countries/US/Ohio/OhioUnemployment/OhioUnemployment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\Ohio\OhioUnemployment;
+
+use Appleton\Taxes\Classes\BaseStateUnemployment;
+
+abstract class OhioUnemployment extends BaseStateUnemployment
+{
+    const TYPE = 'state';
+    const WITHHELD = false;
+}
