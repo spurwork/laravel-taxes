0882eb71d93bcaefb31a6b4e5930ab603df002fb
Aidan Sullivan <aidancs316@gmail.com>
2019-07-19 08:16:16 -0500

    ACC-264 remove unneeded method

--- src/Countries/US/Kentucky/HighlandHeightsCity/V20190101/HighlandHeightsCity.php
+++ src/Countries/US/Kentucky/HighlandHeightsCity/V20190101/HighlandHeightsCity.php
@@ -14,7 +14,11 @@ class HighlandHeightsCity extends BaseHighlandHeightsCity
 
     public function compute(Collection $tax_areas)
     {
-        $this->tax_total = $this->payroll->withholdTax($this->getBaseEarningsWageBase() * static::TAX_RATE);
-        return round($this->tax_total, 2);
+        return round($this->getAdjustedEarnings() * self::TAX_RATE, 2);
+    }
+
+    public function getAdjustedEarnings()
+    {
+        return min($this->payroll->getEarnings(), $this->getBaseEarnings());
     }
 }
