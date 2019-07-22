839d3bea3ebf50d0f4ba63360a97b2ce4f6eb23c
Aidan Sullivan <aidancs316@gmail.com>
2019-07-22 12:12:27 -0500

    ACC-281 update migrations

--- src/migrations/2019_07_19_000001_add_school_district_id_to_ohio_table.php
+++ src/migrations/2019_07_19_000001_add_school_district_id_to_ohio_table.php
@@ -9,7 +9,7 @@ class AddSchoolDistrictIdToOhioTable extends Migration
     public function up()
     {
         Schema::table('ohio_income_tax_information', function (Blueprint $table) {
-            $table->integer('school_district_id')->nullable();
+            $table->string('school_district_id')->nullable();
             $table->string('school_district_name')->nullable();
         });
     }
