681d58a84a50fdaad09300f9932965e63d32d5f1
Aidan Sullivan <aidancs316@gmail.com>
2019-07-29 09:28:24 -0500

    ACC-264 update migration

--- src/migrations/2019_07_18_000001_insert_kentucky_local_taxes_with_cap.php
+++ src/migrations/2019_07_18_000001_insert_kentucky_local_taxes_with_cap.php
@@ -1,5 +1,24 @@
 <?php
 
+use Appleton\Taxes\Countries\US\Kentucky\AlexandriaCity\AlexandriaCity;
+use Appleton\Taxes\Countries\US\Kentucky\AugustaCity\AugustaCity;
+use Appleton\Taxes\Countries\US\Kentucky\BrooksvilleCity\BrooksvilleCity;
+use Appleton\Taxes\Countries\US\Kentucky\BurkesvilleCity\BurkesvilleCity;
+use Appleton\Taxes\Countries\US\Kentucky\CampbellCounty\CampbellCounty;
+use Appleton\Taxes\Countries\US\Kentucky\ClintonCity\ClintonCity;
+use Appleton\Taxes\Countries\US\Kentucky\ColdSpringCity\ColdSpringCity;
+use Appleton\Taxes\Countries\US\Kentucky\CovingtonCity\CovingtonCity;
+use Appleton\Taxes\Countries\US\Kentucky\CrestviewHillsCity\CrestviewHillsCity;
+use Appleton\Taxes\Countries\US\Kentucky\CumberlandCounty\CumberlandCounty;
+use Appleton\Taxes\Countries\US\Kentucky\CumberlandSchoolCounty\CumberlandSchoolCounty;
+use Appleton\Taxes\Countries\US\Kentucky\FortWrightCity\FortWrightCity;
+use Appleton\Taxes\Countries\US\Kentucky\HighlandHeightsCity\HighlandHeightsCity;
+use Appleton\Taxes\Countries\US\Kentucky\McLeanCounty\McLeanCounty;
+use Appleton\Taxes\Countries\US\Kentucky\NelsonCounty\NelsonCounty;
+use Appleton\Taxes\Countries\US\Kentucky\NewportCity\NewportCity;
+use Appleton\Taxes\Countries\US\Kentucky\ParkHillsCity\ParkHillsCity;
+use Appleton\Taxes\Countries\US\Kentucky\RussellCounty\RussellCounty;
+use Appleton\Taxes\Countries\US\Kentucky\WilderCity\WilderCity;
 use Appleton\Taxes\Models\TaxArea;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
@@ -9,25 +28,25 @@ class InsertKentuckyLocalTaxesWithCap extends Migration
     private const STATE_FILE = '2019_07_18_000000_insert_kentucky_local_taxes_with_cap.ini';
 
     private const CLASSES = [
-        'Alexandria City' => Appleton\Taxes\Countries\US\Kentucky\AlexandriaCity\AlexandriaCity::class,
-        'Augusta City' => Appleton\Taxes\Countries\US\Kentucky\AugustaCity\AugustaCity::class,
-        'Brooksville City' => Appleton\Taxes\Countries\US\Kentucky\BrooksvilleCity\BrooksvilleCity::class,
-        'Burkesville City' => Appleton\Taxes\Countries\US\Kentucky\BurkesvilleCity\BurkesvilleCity::class,
-        'Campbell County' => Appleton\Taxes\Countries\US\Kentucky\CampbellCounty\CampbellCounty::class,
-        'Clinton City' => Appleton\Taxes\Countries\US\Kentucky\ClintonCity\ClintonCity::class,
-        'Cold Spring City' => Appleton\Taxes\Countries\US\Kentucky\ColdSpringCity\ColdSpringCity::class,
-        'Covington City' => Appleton\Taxes\Countries\US\Kentucky\CovingtonCity\CovingtonCity::class,
-        'Crestview Hills City' => Appleton\Taxes\Countries\US\Kentucky\CrestviewHillsCity\CrestviewHillsCity::class,
-        'Cumberland County' => Appleton\Taxes\Countries\US\Kentucky\CumberlandCounty\CumberlandCounty::class,
-        'Cumberland School County' => Appleton\Taxes\Countries\US\Kentucky\CumberlandSchoolCounty\CumberlandSchoolCounty::class,
-        'Fort Wright City' => Appleton\Taxes\Countries\US\Kentucky\FortWrightCity\FortWrightCity::class,
-        'Highland Heights City' => Appleton\Taxes\Countries\US\Kentucky\HighlandHeightsCity\HighlandHeightsCity::class,
-        'McLean County' => Appleton\Taxes\Countries\US\Kentucky\McLeanCounty\McLeanCounty::class,
-        'Nelson County' => Appleton\Taxes\Countries\US\Kentucky\NelsonCounty\NelsonCounty::class,
-        'Newport City' => Appleton\Taxes\Countries\US\Kentucky\NewportCity\NewportCity::class,
-        'Park Hills City' => Appleton\Taxes\Countries\US\Kentucky\ParkHillsCity\ParkHillsCity::class,
-        'Russell County' => Appleton\Taxes\Countries\US\Kentucky\RussellCounty\RussellCounty::class,
-        'Wilder City' => Appleton\Taxes\Countries\US\Kentucky\WilderCity\WilderCity::class,
+        'Alexandria City' => AlexandriaCity::class,
+        'Augusta City' => AugustaCity::class,
+        'Brooksville City' => BrooksvilleCity::class,
+        'Burkesville City' => BurkesvilleCity::class,
+        'Campbell County' => CampbellCounty::class,
+        'Clinton City' => ClintonCity::class,
+        'Cold Spring City' => ColdSpringCity::class,
+        'Covington City' => CovingtonCity::class,
+        'Crestview Hills City' => CrestviewHillsCity::class,
+        'Cumberland County' => CumberlandCounty::class,
+        'Cumberland School County' => CumberlandSchoolCounty::class,
+        'Fort Wright City' => FortWrightCity::class,
+        'Highland Heights City' => HighlandHeightsCity::class,
+        'McLean County' => McLeanCounty::class,
+        'Nelson County' => NelsonCounty::class,
+        'Newport City' => NewportCity::class,
+        'Park Hills City' => ParkHillsCity::class,
+        'Russell County' => RussellCounty::class,
+        'Wilder City' => WilderCity::class,
     ];
 
     public function up()
