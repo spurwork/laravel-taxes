8a7d8e802deee4941ad82ea9613adc72eed09e5e
Aidan Sullivan <aidancs316@gmail.com>
2019-08-08 07:52:37 -0500

    ACC-280 fix file names

---
+++ src/Countries/US/California/SacramentoBusinessOperationsTax/SacramentoBusinessOperationsTax.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\California\SacramentoBusinessOperationsTax;
+
+use Appleton\Taxes\Classes\BaseState;
+
+abstract class SacramentoBusinessOperationsTax extends BaseState
+{
+    const WITHHELD = false;
+}
--- src/Countries/US/California/SacramentoBusinessOperationsTax/V20190101/SacramentoBusinessOperationsTax.php
+++ src/Countries/US/California/SacramentoBusinessOperationsTax/V20190101/SacramentoBusinessOperationsTax.php
@@ -1,10 +1,20 @@
 <?php
 
-namespace Appleton\Taxes\Countries\US\California\SanFranciscoPayrollExpenseTax;
+namespace Appleton\Taxes\Countries\US\California\SacrementoBusinessOperationsTax\V20190101;
 
-use Appleton\Taxes\Classes\BaseState;
+use Appleton\Taxes\Countries\US\California\SacrementoBusinessOperationsTax\SacrementoBusinessOperationsTax as BaseSacrementoBusinessOperationsTax;
+use Appleton\Taxes\Traits\HasWageBase;
+use Illuminate\Database\Eloquent\Collection;
 
-abstract class SanFranciscoPayrollExpenseTax extends BaseState
+class SacrementoBusinessOperationsTax extends BaseSacrementoBusinessOperationsTax
 {
-    const WITHHELD = false;
+    // use HasWageBase;
+
+    // const TAX_RATE = 0.001;
+    // const WAGE_BASE = 7000;
+
+    // public function compute(Collection $tax_areas)
+    // {
+    //     return round($this->payroll->withholdTax(min($this->payroll->getEarnings(), $this->getBaseEarnings($tax_areas->first()->workGovernmentalUnitArea)) * self::TAX_RATE), 2);
+    // }
 }
