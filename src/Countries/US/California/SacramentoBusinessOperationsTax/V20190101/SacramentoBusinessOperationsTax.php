9c400b6492e74f0df2331c962f85f226c9786c8c
Aidan Sullivan <aidancs316@gmail.com>
2019-08-15 09:02:28 -0500

    ACC-280 update

--- src/Countries/US/California/SacramentoBusinessOperationsTax/V20190101/SacramentoBusinessOperationsTax.php
+++ src/Countries/US/California/SacramentoBusinessOperationsTax/V20190101/SacramentoBusinessOperationsTax.php
@@ -3,29 +3,29 @@
 namespace Appleton\Taxes\Countries\US\California\SacramentoBusinessOperationsTax\V20190101;
 
 use Appleton\Taxes\Countries\US\California\SacramentoBusinessOperationsTax\SacramentoBusinessOperationsTax as BaseSacramentoBusinessOperationsTax;
-use Appleton\Taxes\Traits\HasWageBase;
 use Illuminate\Database\Eloquent\Collection;
 
 class SacramentoBusinessOperationsTax extends BaseSacramentoBusinessOperationsTax
 {
-    const SINGLE_LIABILITY = 30;
-    const TAX_RATE = 0.0004;
-    const TOTAL_WAGES = 10000;
-    const TOTAL_LIABILITIES = 5000;
+    public const SINGLE_LIABILITY = 30;
+    public const TAX_RATE = 0.0004;
+    public const TOTAL_WAGES = 10000;
+    public const TOTAL_LIABILITIES = 5000;
 
     public function compute(Collection $tax_areas)
     {
-        if ($this->payroll->getYtdEarnings($tax_areas->first()->workGovernmentalUnitArea) === 0) {
-            return $this->payroll->withholdTax(self::SINGLE_LIABILITY);
-        }
-
         if ($this->payroll->getYtdLiabilities($tax_areas->first()->workGovernmentalUnitArea) >= self::TOTAL_LIABILITIES) {
             return 0;
         }
+        // dump($this->payroll->getYtdEarnings($tax_areas->first()->workGovernmentalUnitArea).' ytd earnings');
+        // dump($this->payroll->getYtdEarnings($tax_areas->first()->workGovernmentalUnitArea) >= self::TOTAL_WAGES);
+        // dump($this->payroll->getYtdLiabilities($tax_areas->first()->workGovernmentalUnitArea) < self::TOTAL_LIABILITIES);
+        // dump($this->payroll->getYtdLiabilities($tax_areas->first()->workGovernmentalUnitArea).' ytd liabilities');
+        if ($this->payroll->getYtdEarnings($tax_areas->first()->workGovernmentalUnitArea) >= self::TOTAL_WAGES && $this->payroll->getYtdLiabilities($tax_areas->first()->workGovernmentalUnitArea) < self::TOTAL_LIABILITIES) {
+            // dump($this->payroll->getEarnings());
 
-        if ($this->payroll->getYtdEarnings($tax_areas->first()->workGovernmentalUnitArea) > 0 && $this->payroll->getYtdEarnings($tax_areas->first()->workGovernmentalUnitArea) < self::TOTAL_WAGES) {
-            $tax_total = $this->payroll->withholdTax($this->payroll->getEarnings() * static::TAX_RATE);
-            return round($tax_total, 2);
+            return round($this->payroll->withholdTax($this->payroll->getEarnings() * static::TAX_RATE), 2);
         }
+        dump($this->payroll->getYtdLiabilities($tax_areas->first()->workGovernmentalUnitArea).' ytd liabilities');
     }
 }
