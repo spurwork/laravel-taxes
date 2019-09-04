9a97c8cf7367d7093cb73c6766314f32d7745664
steve <steve@spurjobs.com>
2019-09-04 07:27:10 -0500

    ACC-373: Add tax updates for KY cities

---
+++ src/Countries/US/Kentucky/CamargoCity/V20190701/CamargoCity.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\Kentucky\CamargoCity\V20190701;
+
+use Appleton\Taxes\Countries\US\Kentucky\CamargoCity\CamargoCity as BaseCamargoCity;
+
+class CamargoCity extends BaseCamargoCity
+{
+    public const TAX_RATE = 0.02;
+}
