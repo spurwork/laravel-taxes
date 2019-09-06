05a6b2caa5b5c027e6c117a249aff34cd7f6939c
Richard McDaniel <richard@spurjobs.com>
2019-09-06 09:56:24 -0700

    KY: Special Local Tax Cases (#144)
    * WIP
    
    * Add tests
    
    * Fix Corbin lat/lng
    
    * Add WAGE_START
    
    * Refactor brackets
    
    * Add tax class to closure
    
    * Use parent class name
    
    * Fix zero days worker
    
    * Fix rate
    
    * Fix brackets

---
+++ tests/Unit/Countries/US/Kentucky/CaneyvilleCityTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Appleton\Taxes\Unit\Countries\US\Kentucky;
+
+use Appleton\Taxes\Countries\US\Kentucky\CaneyvilleCity\CaneyvilleCity;
+use Carbon\Carbon;
+use TestCase;
+
+class CaneyvilleCityTest extends TestCase
+{
+    public function testCaneyvilleCity()
+    {
+        Carbon::setTestNow(Carbon::parse('2019-02-01'));
+
+        $results = $this->taxes->calculate(function ($taxes) {
+            $taxes->setHomeLocation($this->getLocation('us.kentucky.caneyville_city'));
+            $taxes->setWorkLocation($this->getLocation('us.kentucky.caneyville_city'));
+            $taxes->setUser($this->user);
+            $taxes->setEarnings(300);
+            $taxes->setPayPeriods(52);
+            $taxes->setDaysWorked(0);
+        });
+
+        $this->assertNull($results->getTax(CaneyvilleCity::class));
+
+        $results = $this->taxes->calculate(function ($taxes) {
+            $taxes->setHomeLocation($this->getLocation('us.kentucky.caneyville_city'));
+            $taxes->setWorkLocation($this->getLocation('us.kentucky.caneyville_city'));
+            $taxes->setUser($this->user);
+            $taxes->setEarnings(300);
+            $taxes->setPayPeriods(52);
+            $taxes->setDaysWorked(3);
+        });
+
+        $this->assertSame(2.00, $results->getTax(CaneyvilleCity::class));
+
+        $results = $this->taxes->calculate(function ($taxes) {
+            $taxes->setHomeLocation($this->getLocation('us.kentucky.caneyville_city'));
+            $taxes->setWorkLocation($this->getLocation('us.kentucky.caneyville_city'));
+            $taxes->setUser($this->user);
+            $taxes->setEarnings(300);
+            $taxes->setPayPeriods(52);
+            $taxes->setDaysWorked(4);
+        });
+
+        $this->assertSame(4.00, $results->getTax(CaneyvilleCity::class));
+    }
+}
