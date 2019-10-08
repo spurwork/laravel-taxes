5cda343e6d75a2aaa37464545c28ca8bb2548deb
Aidan Sullivan <aidancs316@gmail.com>
2019-10-08 13:23:44 -0500

    ACC-435 remove easier for testing

--- src/Countries/US/Oregon/EugeneEmployer/V20190101/EugeneEmployer.php
+++ src/Countries/US/Oregon/EugeneEmployer/V20190101/EugeneEmployer.php
@@ -7,18 +7,8 @@ use Illuminate\Database\Eloquent\Collection;
 
 class EugeneEmployer extends BaseEugeneEmployer
 {
-    // public function compute(Collection $tax_areas)
-    // {
-    //     return 0.0;
-    // }
-
-    // this and below needs to be removed and the commented section above needs to uncommented
-
-    const TAX_RATE = 0.0021;
-
     public function compute(Collection $tax_areas)
     {
-        $this->tax_total = $this->payroll->getEarnings() * self::TAX_RATE;
-        return round($this->tax_total, 2);
+        return 0.0;
     }
 }
