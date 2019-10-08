2ecc6c1d2bd527eccb5d8e313d4b9cf8115c5d31
Aidan Sullivan <aidancs316@gmail.com>
2019-10-08 09:51:08 -0500

    ACC-434 easier testing

--- src/Countries/US/Oregon/Eugene/V20190101/Eugene.php
+++ src/Countries/US/Oregon/Eugene/V20190101/Eugene.php
@@ -9,8 +9,28 @@ use Illuminate\Database\Eloquent\Collection;
 
 class Eugene extends BaseEugene
 {
+    // public function compute(Collection $tax_areas)
+    // {
+    //     return 0.0;
+    // }
+
+    // this and below needs to be removed and the commented section above needs to uncommented
+
+    const TAX_RATE_BETWEEN = 0.003;
+    const TAX_RATE_OVER = 0.0044;
+    const MIN_WAGE = 11.25;
+    const HOURLY_WAGE_CAP = 15.0;
+
     public function compute(Collection $tax_areas)
     {
-        return 0.0;
+        if ($this->payroll->getPayRate() <= self::MIN_WAGE) {
+            return;
+        } elseif ($this->payroll->getPayRate() > self::MIN_WAGE && $this->payroll->getPayRate() < self::HOURLY_WAGE_CAP) {
+            $this->tax_total = $this->payroll->withholdTax($this->payroll->getEarnings() * static::TAX_RATE_BETWEEN);
+        } else {
+            $this->tax_total = $this->payroll->withholdTax($this->payroll->getEarnings() * static::TAX_RATE_OVER);
+        }
+
+        return round($this->tax_total, 2);
     }
 }
