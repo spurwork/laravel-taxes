289011e3cedea4e6ac3000146f0d4c379591e77d
Aidan Sullivan <aidancs316@gmail.com>
2019-12-18 08:12:22 -0600

    ACC-509 better migration

--- src/migrations/2019_11_20_000000_add_parameters_to_federal_income_tax_information.php
+++ src/migrations/2019_11_20_000000_add_parameters_to_federal_income_tax_information.php
@@ -6,15 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 class AddParametersToFederalIncomeTaxInformation extends Migration
 {
+    protected $federal_income_tax_information = 'federal_income_tax_information';
+
     public function up()
     {
-        Schema::table('federal_income_tax_information', function (Blueprint $table) {
+        Schema::table($this->federal_income_tax_information, function (Blueprint $table) {
             $table->integer('other_income')->nullable();
             $table->integer('deductions')->nullable();
             $table->integer('dependents')->nullable();
             $table->integer('extra_withholding')->nullable();
             $table->boolean('step_2_checked')->nullable();
-            $table->string('form_version')->default('2020');
+            $table->string('form_version')->default('2020')->nullable();
+        });
+
+        DB::table($this->federal_income_tax_information)
+            ->whereNull('form_version')
+            ->update([
+                'form_version' => '2019',
+        ]);
+
+        Schema::table($this->federal_income_tax_information, function (Blueprint $table) {
+            $table->string('form_version')->nullable(false)->change();
         });
     }
 }
