ee84f98b50c363f4e3c80f3716b2ef9cb44c072e
Aidan Sullivan <aidancs316@gmail.com>
2020-01-06 14:29:54 -0600

    LAWS-30 update variable

--- src/Countries/US/Oregon/WorkersCompAssessmentFund/V20200101/WorkersCompAssessmentFund.php
+++ src/Countries/US/Oregon/WorkersCompAssessmentFund/V20200101/WorkersCompAssessmentFund.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Eloquent\Collection;
 
 class WorkersCompAssessmentFund extends BaseWorkersCompAssessmentFund
 {
-    const RATE = 1.1;
+    const HOURLY_RATE_MODIFIER = 1.1;
 
     public function compute(Collection $tax_areas)
     {
-        $this->tax_total = $this->payroll->withholdTax($this->payroll->getHoursWorked($tax_areas->first()->workGovernmentalUnitArea) * static::RATE);
+        $this->tax_total = $this->payroll->withholdTax($this->payroll->getHoursWorked($tax_areas->first()->workGovernmentalUnitArea) * static::HOURLY_RATE_MODIFIER);
 
         return round($this->tax_total, 2);
     }
