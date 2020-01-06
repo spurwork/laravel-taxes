46d8387384ce195237666657912b7e29744c4cb0
Aidan Sullivan <aidancs316@gmail.com>
2020-01-06 15:28:08 -0600

    LAWS-32 initial commit

---
+++ src/Countries/US/Oregon/SandyEmployer/SandyEmployer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\Oregon\SandyEmployer;
+
+use Appleton\Taxes\Classes\WorkerTaxes\Taxes\BaseLocal;
+
+abstract class SandyEmployer extends BaseLocal
+{
+    const WITHHELD = false;
+}
