7da31a00759640103bf961ac13948620d4e29c79
Aidan Sullivan <aidancs316@gmail.com>
2020-02-11 15:11:53 -0600

    LAWS-118 update migration better

--- src/migrations/2020_02_11_000000_update_maryland_income_tax_migration.php
+++ src/migrations/2020_02_11_000000_update_maryland_income_tax_migration.php
@@ -7,11 +7,17 @@ class UpdateMarylandIncomeTaxMigration extends Migration
     public function up()
     {
         DB::table('taxes')
-            ->where('class', '=', MarylandIncome::class)
-            ->update(['name' => 'Maryland Income Tax']);
+            ->where('class', 'like', 'Appleton\\\\Taxes\\\\Countries\\\\US\\\\Maryland\\\\MarylandIncome\\\\%')
+            ->orderBy('id')
+            ->each(static function ($tax) {
+                DB::table('taxes')->where('id', $tax->id)->update(['name' => 'Maryland Income Tax']);
+            });
 
         DB::table('taxes')
-            ->where('class', '=', MarylandUnemployment::class)
-            ->update(['name' => 'Maryland Unemployment Tax']);
+            ->where('class', 'like', 'Appleton\\\\Taxes\\\\Countries\\\\US\\\\Maryland\\\\MarylandUnemployment\\\\%')
+            ->orderBy('id')
+            ->each(static function ($tax) {
+                DB::table('taxes')->where('id', $tax->id)->update(['name' => 'Maryland Unemployment Tax']);
+            });
     }
 }
