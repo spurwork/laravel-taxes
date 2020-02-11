8ae0d6bf369d7063f2f0bc08b92939ddab224a79
Aidan Sullivan <aidancs316@gmail.com>
2020-02-11 14:47:41 -0600

    LAWS-129 initial commit

--- src/Countries/US/Utah/UtahIncome/UtahIncome.php
+++ src/Countries/US/Utah/UtahIncome/UtahIncome.php
@@ -8,13 +8,24 @@ use Appleton\Taxes\Models\Countries\US\Utah\UtahIncomeTaxInformation;
 
 abstract class UtahIncome extends BaseStateIncome
 {
-    public const FILING_SINGLE = 'S';
-    public const FILING_MARRIED = 'M';
+    const TYPE = 'federal';
 
-    public const FILING_STATUSES = [
+    const FILING_SINGLE = 0;
+    const FILING_WIDOW = 1;
+    const FILING_HEAD_OF_HOUSEHOLD = 2;
+    const FILING_MARRIED = 3;
+    const FILING_SEPERATE = 4;
+    const FILING_JOINTLY = 5;
+
+    const FILING_STATUSES = [
         self::FILING_SINGLE => 'FILING_SINGLE',
+        self::FILING_WIDOW => 'FILING_WIDOW',
+        self::FILING_HEAD_OF_HOUSEHOLD => 'FILING_HEAD_OF_HOUSEHOLD',
         self::FILING_MARRIED => 'FILING_MARRIED',
+        self::FILING_SEPERATE => 'FILING_SEPERATE',
+        self::FILING_JOINTLY => 'FILING_JOINTLY',
     ];
+
 
     public function __construct(UtahIncomeTaxInformation $tax_information, Payroll $payroll)
     {
---
+++ src/migrations/2020_03_11_000001_add_parameters_to_utah_income_tax_information.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddParametersToUtahIncomeTaxInformation extends Migration
+{
+    protected $utah_income_tax_information = 'utah_income_tax_information';
+
+    public function up()
+    {
+        Schema::table($this->utah_income_tax_information, function (Blueprint $table) {
+            $table->integer('other_income')->nullable();
+            $table->integer('deductions')->nullable();
+            $table->integer('dependents_deduction_amount')->nullable();
+            $table->integer('extra_withholding')->nullable();
+            $table->boolean('step_2_checked')->nullable();
+        });
+    }
+}
