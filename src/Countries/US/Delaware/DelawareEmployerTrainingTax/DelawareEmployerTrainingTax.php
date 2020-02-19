22504048f5a1cfd9375af36a0e7da2cd3930816f
Aidan Sullivan <aidancs316@gmail.com>
2020-02-19 08:47:56 -0600

    LAWS-106 base tax

--- src/Countries/US/Delaware/DelawareEmployerTrainingTax/DelawareEmployerTrainingTax.php
+++ src/Countries/US/Delaware/DelawareEmployerTrainingTax/DelawareEmployerTrainingTax.php
@@ -2,9 +2,9 @@
 
 namespace Appleton\Taxes\Countries\US\Delaware\DelawareEmployerTrainingTax;
 
-use Appleton\Taxes\Classes\WorkerTaxes\Taxes\BaseTax;
+use Appleton\Taxes\Classes\WorkerTaxes\Taxes\BaseState;
 
-abstract class DelawareEmployerTrainingTax extends BaseTax
+abstract class DelawareEmployerTrainingTax extends BaseState
 {
     const TYPE = 'state';
     const WITHHELD = true;
