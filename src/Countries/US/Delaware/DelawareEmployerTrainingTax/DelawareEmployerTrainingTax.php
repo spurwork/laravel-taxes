d84811b3dcdc4413043bd74ddd13385e106cd163
Aidan Sullivan <aidancs316@gmail.com>
2020-02-26 10:25:51 -0600

    LAWS-106 update class

--- src/Countries/US/Delaware/DelawareEmployerTrainingTax/DelawareEmployerTrainingTax.php
+++ src/Countries/US/Delaware/DelawareEmployerTrainingTax/DelawareEmployerTrainingTax.php
@@ -2,9 +2,10 @@
 
 namespace Appleton\Taxes\Countries\US\Delaware\DelawareEmployerTrainingTax;
 
-use Appleton\Taxes\Classes\WorkerTaxes\Taxes\BaseState;
+use Appleton\Taxes\Classes\WorkerTaxes\Taxes\BaseStateUnemployment;
 
-abstract class DelawareEmployerTrainingTax extends BaseState
+abstract class DelawareEmployerTrainingTax extends BaseStateUnemployment
 {
+    const TYPE = 'state';
     const WITHHELD = false;
 }
