eb65e40f967c139cfaf922930c6c2db3fdbc0162
Aidan Sullivan <aidancs316@gmail.com>
2020-03-18 11:43:34 -0500

    LAWS-173 update la taxes

--- src/Models/Countries/US/Louisiana/LouisianaIncomeTaxInformation.php
+++ src/Models/Countries/US/Louisiana/LouisianaIncomeTaxInformation.php
@@ -15,6 +15,7 @@ class LouisianaIncomeTaxInformation extends BaseTaxInformationModel
         $tax_information->exemptions = 0;
         $tax_information->exempt = false;
         $tax_information->dependents = 0;
+        $tax_information->additional_withholding = 0;
         $tax_information->filing_status = LouisianaIncome::FILING_SINGLE;
 
         return $tax_information;
