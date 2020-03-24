a6ee3eea8407966caee14c0b03553855a04cd2b2
Aidan Sullivan <aidancs316@gmail.com>
2020-03-24 09:54:49 -0500

    LAWS-105 update

--- src/Classes/WorkerTaxes/Payroll.php
+++ src/Classes/WorkerTaxes/Payroll.php
@@ -162,6 +162,23 @@ class Payroll
         );
     }
 
+    public function payrollCrossesMonths()
+    {
+        $start_date = $this->start_date->copy();
+        $end_date = $this->end_date->copy();
+        $start_of_month = $this->start_date->copy()->startOfMonth();
+        $start_of_next_month = $this->start_date->addMonth()->copy()->startOfMonth();
+        $start_of_year = $this->start_date->copy()->startOfYear();
+
+        if ($start_date < $start_of_year) {
+            return true;
+        } elseif ($start_date < $start_of_next_month && $end_date >= $start_of_next_month) {
+            return true;
+        }
+
+        return false;
+    }
+
     public function getMtdEarnings(GovernmentalUnitArea $governmental_unit_area = null): float
     {
         if ($governmental_unit_area === null) {
@@ -177,7 +194,7 @@ class Payroll
 
         return $this->wage_manager->calculateEarnings(
             $area_income->getHistoricalWages(),
-            $start_of_year->min($this->start_date->min($start_of_month))
+            $start_of_month < $start_of_year ? $start_of_year : $start_of_month
         );
     }
 
@@ -194,7 +211,7 @@ class Payroll
 
         return $this->wage_manager->calculateEarnings(
             $area_income->getHistoricalWages(),
-            $this->start_date->min($this->start_date->copy()->startOfYear())
+            $this->start_date->copy()->startOfYear()
         );
     }
 
