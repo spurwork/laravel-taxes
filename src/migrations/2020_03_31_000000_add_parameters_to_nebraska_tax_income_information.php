eb6da3e4ef53f2831801ec27a16092cfee1efdf5
Aidan Sullivan <aidancs316@gmail.com>
2020-03-31 09:40:51 -0500

    LAWS-196 ne updates

---
+++ src/migrations/2020_03_31_000000_add_parameters_to_nebraska_tax_income_information.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddParametersToNebraskaTaxIncomeInformation extends Migration
+{
+    protected $nebraska_income_tax_information = 'nebraska_income_tax_information';
+
+    public function up()
+    {
+        Schema::table($this->nebraska_income_tax_information, function (Blueprint $table) {
+            $table->integer('additional_withholding')->default(0);
+        });
+    }
+}
