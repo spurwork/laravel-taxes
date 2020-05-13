6f2acff97938c92ad0a275ac2d20b7ed6da8ef64
Aidan Sullivan <aidancs316@gmail.com>
2020-05-13 10:16:29 -0500

    LAWS-249 updated migration

--- src/migrations/2020_05_08_000000_add_parameters_to_pennsylvania_income_tax_information.php
+++ src/migrations/2020_05_08_000000_add_parameters_to_pennsylvania_income_tax_information.php
@@ -11,9 +11,13 @@ class AddParametersToPennsylvaniaIncomeTaxInformation extends Migration
         Schema::table('pennsylvania_income_tax_information', function (Blueprint $table) {
             $table->float('employer_eit_rate')->nullable();
             $table->float('resident_eit_rate')->nullable();
-            $table->string('resident_psd_code')->default('');
-            $table->string('employer_psd_code')->default('');
-            $table->boolean('exempt_from_lst')->default(false);
+            $table->boolean('is_resident_psd_code_philadelphia')->default(false);
+            $table->boolean('is_employer_psd_code_philadelphia')->default(false);
+            $table->integer('municipal_lst_total')->nullable();
+            $table->integer('school_district_lst_total')->nullable();
+            $table->integer('municipal_lst_lie_total')->nullable();
+            $table->integer('school_district_lst_lie_total')->nullable();
+            $table->integer('lst_paid_to_previous_employers')->nullable();
         });
     }
 }
