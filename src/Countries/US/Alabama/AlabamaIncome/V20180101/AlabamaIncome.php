8a5c8d01522f0a95d1dedc58a883d1c8d0166e1c
steve <steve@spurstaffing.com>
2023-01-30 15:07:16 -0600

    SS-944 - fix 2018 al taxes

--- src/Countries/US/Alabama/AlabamaIncome/V20180101/AlabamaIncome.php
+++ src/Countries/US/Alabama/AlabamaIncome/V20180101/AlabamaIncome.php
@@ -10,11 +10,53 @@ use Appleton\Taxes\Models\Countries\US\Alabama\AlabamaIncomeTaxInformation;
 
 class AlabamaIncome extends BaseAlabamaIncome
 {
-    const SUPPLEMENTAL_TAX_RATE = AlabamaIncome2017::SUPPLEMENTAL_TAX_RATE;
+    const SUPPLEMENTAL_TAX_RATE = 0.05;
+
     const SINGLE_BRACKETS = AlabamaIncome2017::SINGLE_BRACKETS;
+
     const MARRIED_BRACKETS = AlabamaIncome2017::MARRIED_BRACKETS;
-    const STANDARD_DEDUCTIONS = AlabamaIncome2017::STANDARD_DEDUCTIONS;
+
+    const STANDARD_DEDUCTIONS = [
+        self::FILING_SINGLE => [
+            'base' => 23499.99,
+            'amount' => 2500,
+            'floor' => 2000,
+            'modifier' => [
+                'amount' => 25,
+                'per' => 500,
+            ],
+        ],
+        self::FILING_SEPERATE => [
+            'base' => 10749.99,
+            'amount' => 3750,
+            'floor' => 2000,
+            'modifier' => [
+                'amount' => 88,
+                'per' => 250,
+            ]
+        ],
+        self::FILING_MARRIED => [
+            'base' => 23499.99,
+            'amount' => 7500,
+            'floor' => 4000,
+            'modifier' => [
+                'amount' => 175,
+                'per' => 500,
+            ]
+        ],
+        self::FILING_HEAD_OF_HOUSEHOLD => [
+            'base' => 23499.99,
+            'amount' => 4700,
+            'floor' => 2000,
+            'modifier' => [
+                'amount' => 135,
+                'per' => 500,
+            ]
+        ],
+    ];
+
     const PERSONAL_EXEMPTION_ALLOWANCES = AlabamaIncome2017::PERSONAL_EXEMPTION_ALLOWANCES;
+
     const DEPENDENT_EXEMPTION_BRACKETS = AlabamaIncome2017::DEPENDENT_EXEMPTION_BRACKETS;
 
     public function __construct(AlabamaIncomeTaxInformation $tax_information, FederalIncome $federal_income, Payroll $payroll)
