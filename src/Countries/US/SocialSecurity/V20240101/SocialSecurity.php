64c25bb889e5ffab72c045c6cbdc31095f44f2c4
steve <steve.daily@spurwork.com>
2024-04-11 09:02:40 -0500

    SS-1131 - 2024 social security tax

---
+++ src/Countries/US/SocialSecurity/V20240101/SocialSecurity.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Appleton\Taxes\Countries\US\SocialSecurity\V20240101;
+
+use Appleton\Taxes\Countries\US\SocialSecurity\SocialSecurity as BaseSocialSecurity;
+
+class SocialSecurity extends BaseSocialSecurity
+{
+    public const TAX_RATE = 0.062;
+    public const WAGE_BASE = 168600;
+}
---
+++ tests/Unit/Countries/US/SocialSecurity/V20240101/SocialSecurityTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Appleton\Taxes\Tests\Unit\Countries\US\SocialSecurity\V20240101;
+
+use Appleton\Taxes\Countries\US\SocialSecurity\SocialSecurity;
+use Appleton\Taxes\Tests\Unit\Countries\TestParameters;
+use Appleton\Taxes\Tests\Unit\Countries\TestParametersBuilder;
+use Appleton\Taxes\Tests\Unit\Countries\WageBaseTaxTestCase;
+
+class SocialSecurityTest extends WageBaseTaxTestCase
+{
+    private const DATE = '2024-01-01';
+    private const LOCATION = 'us';
+    private const TAX_CLASS = SocialSecurity::class;
+    private const WAGE_BASE_IN_CENTS = 16860000;
+    private const TAX_RATE = 0.062;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->query_runner->addTax(self::TAX_CLASS);
+    }
+
+    /**
+     * @dataProvider provideTestData
+     */
+    public function testTax(TestParameters $parameters): void
+    {
+        $this->validate($parameters);
+    }
+
+    /**
+     * @dataProvider provideWageBaseTestData
+     */
+    public function testWageBase(TestParameters $parameters): void
+    {
+        $this->validateWageBase($parameters);
+    }
+
+    public function provideTestData(): array
+    {
+        $builder = new TestParametersBuilder();
+        $builder
+            ->setDate(self::DATE)
+            ->setHomeLocation(self::LOCATION)
+            ->setTaxClass(self::TAX_CLASS)
+            ->setPayPeriods(52);
+
+        return [
+            'case study A' => [
+                $builder
+                    ->setWagesInCents(64000)
+                    ->setYtdLiabilitiesInCents(0)
+                    ->setExpectedAmountInCents(3968)
+                    ->setExpectedEarningsInCents(64000)
+                    ->build()
+            ],
+            'case study B' => [
+                $builder
+                    ->setWagesInCents(77428)
+                    ->setYtdLiabilitiesInCents(0)
+                    ->setExpectedAmountInCents(4801)
+                    ->setExpectedEarningsInCents(77428)
+                    ->build()
+            ],
+            'case study C' => [
+                $builder
+                    ->setWagesInCents(64000)
+                    ->setYtdLiabilitiesInCents(13300000)
+                    ->setExpectedAmountInCents(3968)
+                    ->setExpectedEarningsInCents(64000)
+                    ->build()
+            ],
+            'case study D' => [
+                $builder
+                    ->setWagesInCents(77428)
+                    ->setYtdLiabilitiesInCents(13300000)
+                    ->setExpectedAmountInCents(4801)
+                    ->setExpectedEarningsInCents(77428)
+                    ->build()
+            ],
+        ];
+    }
+
+    public function provideWageBaseTestData(): array
+    {
+        return $this->wageBaseBoundariesTestCases(
+            self::DATE,
+            self::LOCATION,
+            self::TAX_CLASS,
+            self::WAGE_BASE_IN_CENTS,
+            self::TAX_RATE
+        );
+    }
+}
